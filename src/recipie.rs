72b6ea94eabaadd4e6b18e0968acbc2250627057
Nicholas Grigorian <grigoriannick@gmail.com>
2025-11-18 15:32:53 -0500

    Expanded some functionality

--- src/recipie.rs
+++ src/recipie.rs
@@ -1,7 +1,25 @@
 use crate::ingredients::{IngredientSelector};
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, PartialEq)]
+enum Measure {
+    oz(f32),
+    shot(f32),
+    taste
+}
+
+#[derive(Serialize, Deserialize)]
+struct Component {
+    ingredient: IngredientSelector,
+    amount: Measure
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct Recipie {
-    ingredients: Vec<IngredientSelector>
+    name: String,
+    components: Vec<Component>
+}
+
+impl Recipie {
+
 }
--- src/recipie_store.rs
+++ src/recipie_store.rs
@@ -1,28 +1,57 @@
 
-use std::{collections::HashMap, default, fs::File, path::PathBuf};
+use std::{collections::HashMap, default, fs::File, io::BufReader, path::PathBuf};
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 use crate::{ingredients::IngredientSelector, recipie::Recipie};
 
 #[derive(Serialize, Deserialize, Default)]
 pub struct RecipieStore {
-    recipies: HashMap<uuid::Uuid, Recipie>
+    recipies: HashMap<uuid::Uuid, Recipie>,
+    config: Option<PathBuf>
 }
 
 impl RecipieStore {
+    // Tries to deserialize from a common json file, otherwise it only populates in memory
     pub fn new() -> RecipieStore {
-        match dirs::config_local_dir() {
-            Some(path) => Self::from_config(path),
-            _ => Self::from_config(PathBuf::from("."))
+        let cfg_dir = dirs::config_local_dir().unwrap_or(PathBuf::from("."));
+        match Self::from_config(cfg_dir)
+        {
+            Ok(store) => store,
+            Err(_) => RecipieStore::default()
         }
     }
 
-    pub fn from_config(config_dir: PathBuf) -> RecipieStore {
-        match File::open(config_dir.join("recipies.json"))
-        {
-            Ok(fs) => serde_json::from_reader(fs).unwrap_or_default(),
-            Err(_) => RecipieStore::default(),
+    // Loads from the specified config
+    pub fn from_config(config_dir: PathBuf) -> Result<RecipieStore, Box<dyn std::error::Error>> {
+        let path = config_dir.join("recipies.json");
+        if std::fs::exists(&path)? {
+            let cfg_file = File::open(path)?;
+            let cfg = BufReader::new(cfg_file);
+            let store = serde_json::from_reader(cfg)?;
+            Ok(store)
+        }
+        else {
+            std::fs::File::create_new(&path)?;
+            let mut store = RecipieStore::default();
+            store.config = Some(path);
+            Ok(store)
+        }
+    }
+
+    // Saves to whatever config this was loaded from
+    pub fn save(&self) -> bool {
+        match &self.config {
+            Some(path) => self.save_to(path),
+            _ => false
+        }
+    }
+
+    // Saves to a specific file
+    pub fn save_to(&self, path: &PathBuf) -> bool {
+        match File::create(path) {
+            Ok(file) => serde_json::to_writer(file, self).is_ok(),
+            _ => false
         }
     }
 }
