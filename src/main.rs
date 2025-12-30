e8934746cf3fba0b0489adf2e5700614363cf5d4
Nicholas Grigorian <grigoriannick@gmail.com>
2025-12-30 14:48:57 -0500

    Added in app icon

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,27 @@ mod ingredient_store;
 mod ingredient_selector_builder;
 mod component_builder;
 
+use std::io::Read;
+
 use eframe::egui;
+use egui::{IconData, Image, ImageSource, Style, Visuals, emath::interpolation_factor, epaint::image, include_image};
+use ::image::load_from_memory;
 
 fn main() -> eframe::Result {
+    let icon_bytes = include_bytes!("../icon.png");
+    let (rgba, width, height) = {
+        let img = load_from_memory(icon_bytes).expect("Missing icon.png!");
+        (img.as_rgba8().expect("Cannot convert icon to rgba8!").to_vec(), img.width(), img.height())
+    };
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default(),
+        viewport: egui::ViewportBuilder::default()
+            .with_icon(std::sync::Arc::new(
+                IconData {
+                    rgba,
+                    width,
+                    height
+                }
+            )),
         ..Default::default()
     };
     eframe::run_native(
